3686fd39c79c42af9fb7d624c90c1242df7f99f9
Pogrebniak, Anna (k1895827) <anna.pogrebniak@kcl.ac.uk>
2020-06-07 19:05:22 +0100

    website updates

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask , render_template, url_for, request
+import csv
+app = Flask(__name__)
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        uname = data["uname"]
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([uname, email, subject, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return render_template('/thankyou.html')
+    else:
+        return 'Something is wrong'
